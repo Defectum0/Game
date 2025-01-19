7d4e14fa0336ba0cf07209422ca4066cfdfb4aa5
dedbo <dedbo@DESKTOP-KG5AOOK>
2025-01-19 22:37:00 +0300

    Добавьте файлы проекта.

---
+++ Игра/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Игра
+{
+    public class Player
+    {
+       
+        
+       public char Symbol { get; }
+       public Player(char symbol)
+       {
+          Symbol = symbol;
+       }
+        
+
+    }
+}
